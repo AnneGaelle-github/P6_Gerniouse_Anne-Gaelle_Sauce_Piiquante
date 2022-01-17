d04bfb73233bcaaa639e42dcfdde682c0bf6c559
John Doe <johndoe@example.com>
2022-01-17 10:23:11 +0100

    Création de l'application express : app.js

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+// Application Express
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+const stuffRoutes = require('./routes/stuff');
+
+
+// Connexion à la base de données MongoDB Atlas
+
+mongoose.connect('mongodb+srv://tmp:tmp@cluster0.mm3fb.mongodb.net/test?retryWrites=true&w=majority', 
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.error('Connexion à MongoDB échouée !'));
+
+
+const app = express();
+
+// Les CORS qui gère les requettes HTTP
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json()); // Prend toutes les requêtes qui ont comme Content-Type du JSON
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
