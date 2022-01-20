fbb90f639225d9d978b01a2f6dcdd9657e4d4565
John Doe <johndoe@example.com>
2022-01-20 14:47:58 +0100

    Modification du TOKEN auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]; // 'authorization' extrai le token du header et 'split' le récupère
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // 'verify' décode le token
+    const decodedToken = jwt.verify(token, '${process.env.TOKEN}'); // 'verify' décode le token
     const userId = decodedToken.userId;
     req.auth = { userId };
     console.log(req.auth);
